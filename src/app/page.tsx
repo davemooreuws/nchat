9031fec5b0224a0343d0ef6e3e53df9c52fca75c
David Moore <davemooreuws@gmail.com>
2023-08-10 14:22:01 +1000

    adjust auth error conditional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,29 +41,27 @@ export default async function Home() {
 
   return (
     <div className='flex flex-col bg-cover'>
-      {!websocketUrl ? (
-        <div className='max-w-4xl mx-auto p-6'>
-          There was a problem loading the messages, please login again.
+      <SignedIn>
+        {!websocketUrl ? (
+          <div className='max-w-4xl mx-auto p-6'>
+            There was a problem loading the messages, please login again.
+          </div>
+        ) : (
+          <div className='flex-1 overflow-y-scroll no-scrollbar p-6'>
+            <div className='max-w-4xl mx-auto'>
+              <MessageFeed
+                messages={initialMessages}
+                websocketUrl={websocketUrl}
+              />
+            </div>
+          </div>
+        )}
+      </SignedIn>
+      <SignedOut>
+        <div className='h-full flex items-center justify-center flex-col py-10'>
+          <SignIn />
         </div>
-      ) : (
-        <>
-          <SignedIn>
-            <div className='flex-1 overflow-y-scroll no-scrollbar p-6'>
-              <div className='max-w-4xl mx-auto'>
-                <MessageFeed
-                  messages={initialMessages}
-                  websocketUrl={websocketUrl}
-                />
-              </div>
-            </div>
-          </SignedIn>
-          <SignedOut>
-            <div className='h-full flex items-center justify-center flex-col py-10'>
-              <SignIn />
-            </div>
-          </SignedOut>
-        </>
-      )}
+      </SignedOut>
     </div>
   );
 }
